3baca05501fbb3de51a08beefb96a05aafde944c
Rick Box <rick.box@bbc.co.uk>
2017-02-20 12:36:01 +0000

    Adds in option to modify DNS - removes hardcoded values
    Some networks may not allow 8.8.8.8 Google DNS servers. This patch
    allows this to be specified in the variables file or to pull the value
    from the systems resolv.conf file.
    
    Change-Id: I494ab82c41bcd59ea85404d49cb2696029353a0e

--- multi-node-aio/variables.sh
+++ multi-node-aio/variables.sh
@@ -1,2 +1,6 @@
 # Variables used by multi-node-aio
+# Network subnet used for all the virtual machines
 NETWORK_BASE=172.29
+# DNS used throughout the deploy
+#DNS_NAMESERVER=$(cat /etc/resolv.conf | grep -m 1 "nameserver" | sed "s/nameserver //")
+DNS_NAMESERVER=8.8.8.8
