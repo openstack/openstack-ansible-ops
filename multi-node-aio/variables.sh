d38b7b059afae171045e0cf0494caa24434085c0
Rick Box <rick.box@bbc.co.uk>
2017-02-20 15:32:15 +0000

    Adds in option to disable overriding of apt-sources list
    By default AIO deploy overrides apt-sources, if things like a local
    mirror are already set up then this script will override these. This
    option allows for the override to be disabled.
    
    Change-Id: Id84c2c5d12153125d7ac6698493c41a840993c5b

--- multi-node-aio/variables.sh
+++ multi-node-aio/variables.sh
@@ -4,3 +4,8 @@ NETWORK_BASE=172.29
 # DNS used throughout the deploy
 #DNS_NAMESERVER=$(cat /etc/resolv.conf | grep -m 1 "nameserver" | sed "s/nameserver //")
 DNS_NAMESERVER=8.8.8.8
+
+# By default AIO deploy overrides apt-sources, if things like a local mirror are already
+# set up then this script will override these. This option allows for the override to be
+# disabled.
+OVERRIDE_SOURCES=true
