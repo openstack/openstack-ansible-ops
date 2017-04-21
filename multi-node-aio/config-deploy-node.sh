3dcb39b79dafbdc14e75c7492f05a0bdf4f771cf
Melissa Kam <melissa.kam@rackspace.com>
2017-04-21 15:51:32 -0500

    Fix variable exports for deployment
    The newline was preventing some of the variables from
    being exported properly.
    
    Change-Id: If189f6e288796d828da749c5873983ea46764068

--- multi-node-aio/config-deploy-node.sh
+++ multi-node-aio/config-deploy-node.sh
@@ -13,8 +13,8 @@ scp -r -o StrictHostKeyChecking=no  ~/.ssh deploy1:/root/
 scp -r -o StrictHostKeyChecking=no  ../multi-node-aio deploy1:/root
 
 # Deploy openstack-ansible from deploy node and export all variables deploy-osa.sh needs
-ssh -o StrictHostKeyChecking=no deploy1 "export NETWORK_BASE=${NETWORK_BASE} RUN_OSA=${RUN_OSA}
-OSA_BRANCH=${OSA_BRANCH} PRE_CONFIG_OSA=${PRE_CONFIG_OSA}; apt update; cd /root/multi-node-aio/; ./deploy-osa.sh"
+ssh -o StrictHostKeyChecking=no deploy1 "export NETWORK_BASE=${NETWORK_BASE} RUN_OSA=${RUN_OSA} " \
+"OSA_BRANCH=${OSA_BRANCH} PRE_CONFIG_OSA=${PRE_CONFIG_OSA}; apt update; cd /root/multi-node-aio/; ./deploy-osa.sh"
 
 # Add 2222 rules to iptables for ssh directly into deployment node.
 iptables_filter_rule_add nat 'PREROUTING -p tcp --dport 2222 -j DNAT --to 10.0.0.150:22'
