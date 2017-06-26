4dee5bcb0e9c5698bdd1a6693ffb76df2a6ab254
ZhongShengping <chdzsp@163.com>
2017-06-26 14:33:11 +0800

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work[0] for Pike we need to switch to use the
    openstackdocstheme.
    
    [0]https://review.openstack.org/#/c/472275/
    
    Change-Id: I3fe77c8400428d74e733e5eefd5bf5bea39ac230

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -22,6 +22,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import openstackdocstheme
 import pbr.version
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -38,7 +39,6 @@ import pbr.version
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'oslosphinx',
     'reno.sphinxext',
 ]
 
@@ -121,7 +121,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -129,7 +129,7 @@ html_theme = 'default'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+html_theme_path = [openstackdocstheme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
