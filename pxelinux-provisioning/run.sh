743b939640f3ad314b806e86ac1e941a617e4069
Kevin Carter <kevin.carter@rackspace.com>
2018-04-26 10:10:26 -0500

    Add basic provisioning using pxelinux
    The basic provisioning tools we had in the MNAIO could long be used on a
    set of physical machines however doing so required a healthy
    understanding of everything going on under the hood. This change
    extracts the PXE components out of our older MNAIO tooling and
    will allow operators to easily deploy operating systems on machines in
    the most compatible way possible.
    
    Change-Id: I2188f0f0de7f8be331a35b5f22cf5114ea9b6718
    Signed-off-by: Kevin Carter <kevin.carter@rackspace.com>

---
+++ pxelinux-provisioning/run.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# Copyright [2016] [Kevin Carter]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euvo
+
+source ansible-env.rc
+
+ansible-playbook -vv \
+                 -i ${DEFAULT_INVENTORY:-"playbooks/inventory.yml"} \
+                 -e setup_host=${SETUP_HOST:-"true"} \
+                 -e setup_pxeboot=${SETUP_PXEBOOT:-"true"} \
+                 -e setup_dhcpd=${SETUP_DHCPD:-"true"} \
+                 -e default_image=${DEFAULT_IMAGE:-"ubuntu-16.04-amd64"} \
+                 -e default_http_proxy=${DEFAULT_HTTP_PROXY:-''} \
+                 --force-handlers \
+                 playbooks/site.yml
