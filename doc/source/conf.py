cc4b993b210ae0dd3f654acf0798615f3c59f603
Mohammed Naser <mnaser@vexxhost.com>
2019-04-13 19:30:11 -0400

    doc: drop release notes + fix sphinxmark
    The ops repo isn't really software.   It's a collection of playbooks
    that we share between operators, therefore, release notes are a bit
    confusing because there are so many playbooks inside of it.
    
    They're also broken because of how we have a few links all over the
    place.  There has been no release notes published either.
    
    sphinxmark is no longer compatible with the latest release of Sphinx
    which is causing all of our documentation jobs to fail.  This patch
    removes it as our current usage of openstacktheme for documentation
    already provides watermarks for current branch and notices for which
    branch the documentation covers.
    
    Change-Id: I521647c999e7dc17ba27f02bc5bccd422d97ab9a
    Closes-Bug: #1824664

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,7 +23,6 @@
 # serve to show the default.
 
 import pbr.version
-import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -41,7 +40,6 @@ import os
 extensions = [
     'openstackdocstheme',
     'sphinx.ext.autodoc',
-    'sphinxmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -312,17 +310,3 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
-
-
-watermark = os.popen("git branch --contains $(git rev-parse HEAD)\
-| awk -F/ '/stable/ {print $2}'").read().strip(' \n\t').capitalize()
-if watermark == "":
-    watermark = "Pre-release"
-
-# -- Options for sphinxmark -----------------------------------------------
-sphinxmark_enable = True
-sphinxmark_div = 'docs-body'
-sphinxmark_image = 'text'
-sphinxmark_text = watermark
-sphinxmark_text_color = (128, 128, 128)
-sphinxmark_text_size = 70
